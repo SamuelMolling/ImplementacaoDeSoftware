ad8af53621a053b91f0782cb72c41be49d0f2c63
Samuel Molling <samuel.molling@ifood.com.br>
2022-08-24 08:53:21 -0300

    first-commit

---
+++ db/db.py
@@ -0,0 +1,45 @@
+import sqlalchemy as db
+import sqlalchemy.orm as orm
+
+engine = db.create_engine('sqlite:///bank.db')
+conn = engine.connect()
+Base = orm.declarative_base()
+
+# Client table
+class Client(Base):
+  __tablename__ = 'client'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), nullable=False)
+  sex = db.Column(db.String(1), nullable=False)
+  cpf = db.Column(db.String(11), nullable=False)
+  birth_date = db.Column(db.Date, nullable=False)
+
+# Bank Account table 
+class BankAccount(Base):
+  __tablename__ = 'bank_account'
+  id = db.Column(db.Integer, primary_key=True)
+  client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
+  account_type = db.Column(db.Integer, db.ForeignKey('account_type.id'))
+  balance = db.Column(db.Float, nullable=False)
+
+# Account Type table 
+class AccountType(Base):
+  __tablename__ = 'account_type'
+  id = db.Column(db.Integer, primary_key=True)
+  type = db.Column(db.String(10), nullable=False)
+
+# Movimentation table 
+class Movimentation(Base):
+  __tablename__ = 'movimentation'
+  id = db.Column(db.Integer, primary_key=True)
+  type = db.Column(db.Integer, db.ForeignKey('movimentation_type.id'))
+  account = db.Column(db.Integer, db.ForeignKey('bank_account.id'))
+  date = db.Column(db.DateTime, nullable=False)
+  value = db.Column(db.Float, nullable=False)
+
+
+# Movimentation type table 
+class MovimentationType(Base):
+  __tablename__ = 'movimentation_type'
+  id = db.Column(db.Integer, primary_key=True)
+  type = db.Column(db.String(50), nullable=False)
