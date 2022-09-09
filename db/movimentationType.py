70048cd054725bb19d2be99a0a461a829a99d47e
Samuel Molling <samuel.molling@ifood.com.br>
2022-09-09 19:58:52 -0300

    add-validation-if-exist

--- db/movimentationType.py
+++ db/movimentationType.py
@@ -4,23 +4,26 @@ Session = orm.sessionmaker(bind=engine)
 session = Session()
 engine.echo=True
 
+# Validate if the Movimentation Type is already registered
+rows = session.query(MovimentationType).all()
+if rows > 3:
 # Insert
-try:
+    try:
 
-    default = [
-        MovimentationType(
-            type='deposito'),
-        MovimentationType(
-            type='saque'),
-        MovimentationType(
-            type='juros')
-    ]
+        default = [
+            MovimentationType(
+                type='deposito'),
+            MovimentationType(
+                type='saque'),
+            MovimentationType(
+                type='juros')
+        ]
 
-    session.add_all(default)
-    session.commit()
-except Exception as e:
-    session.rollback()
-    print(e)
+        session.add_all(default)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e)
 
 # Close connection with database
 session.close()
