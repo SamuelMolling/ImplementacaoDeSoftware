841432079c131e1d55922479d6c3e9e25920628d
Samuel Molling <samuel.molling@ifood.com.br>
2022-09-15 14:46:20 -0300

    ok

--- pages/costumerWindow.py
+++ pages/costumerWindow.py
@@ -17,50 +17,56 @@ class createCostumerWindow(tk.Tk):
     'cpf': tk.StringVar(),
     'birthday': tk.StringVar()
   }
+  self.name = tk.StringVar(),
+  self.sex = tk.StringVar(),
+  self.cpf = tk.StringVar(),
+  self.birthday = tk.StringVar()
 
   def destroy():
     op = messagebox.askyesno("Exit","Do you want to exit?")
     if op>0:
         self.destroy()
 
-  def newCcostumer():
-    for k, v in self.client.items():
-      setattr(self.client, k, v.get())
-      print(k, v.get())
+  def newCostumer():
+    name = self.name
+    sex = self.sex
+    cpf = self.cpf
+    birthday = self.birthday
+    newClient(name, sex, cpf, birthday)
 
-    # name = self.name.get()
-    # sex = self.sex.get()
-    # cpf = self.cpf.get()
-    # birthday = self.birthday.get()
-    # newClient(name, sex, cpf, birthday)
+  # # Id
+  # self.label = tk.Label(self, text="Id", state=tk.DISABLED)
+  # self.entry = tk.Entry(self, state=tk.DISABLED)
+  # self.label.pack()
+  # self.entry.pack()
 
   # Name
   self.label = tk.Label(self, text="Name")
-  self.entry = tk.Entry(self, textvariable=self.client.get('name'))
+  self.entry = tk.Entry(self, textvariable=self.name)
   self.label.pack()
   self.entry.pack()
 
   # Sex
   self.label = tk.Label(self, text="Sex")
-  self.entry = ttk.Combobox(self, textvariable=self.client.get('sex'))
+  self.entry = ttk.Combobox(self, state='readonly',textvariable=self.sex)
   self.entry['values'] = ('M','F')
   self.label.pack()
   self.entry.pack()
 
   # CPF
   self.label = tk.Label(self, text="CPF")
-  self.entry = tk.Entry(self, textvariable=self.client.get('cpf'))
+  self.entry = tk.Entry(self, textvariable=self.cpf)
   self.label.pack()
   self.entry.pack()
 
   # Birthday
   self.label = tk.Label(self, text="Birthday")
-  self.entry = DateEntry(self,selectmode='day', textvariable=self.client.get('birthday'))
+  self.entry = DateEntry(self,selectmode='day', textvariable=self.birthday)
   self.label.pack()
   self.entry.pack()
 
   # Button for save
-  self.button = tk.Button(self, text="Save", command=newCcostumer)
+  self.button = tk.Button(self, text="Save", command=newCostumer)
   self.button.pack()
 
   # Quit
