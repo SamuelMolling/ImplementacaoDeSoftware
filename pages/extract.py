87b34fa1caf1b717a1c7445c8d9f65334b3a395f
Samuel Molling <samuel.molling@ifood.com.br>
2022-09-18 20:32:26 -0300

    falta-extrato

--- pages/extract.py
+++ pages/extract.py
@@ -14,31 +14,44 @@ def ExtractWindow(costumer, account, movimentations):
     window.geometry('800x800')
 
     ##### Cabeçalho #####
-    Label(window, text="Costumer information").pack()
-    costumerInformationColumn = ('Name', 'CPF', 'Sex', 'Birthday')
-    tree = ttk.Treeview(window, columns=costumerInformationColumn, height=50, selectmode="extended", show='headings')
-    tree.heading('Name', text="Name", anchor=W)
-    tree.heading('CPF', text="CPF", anchor=W)
-    tree.heading('Sex', text="Sex", anchor=W)
-    tree.heading('Birthday', text="Birthday", anchor=W)
 
-    for x in costumer:
-        tree.insert('', 'end', values=(x[0], x[1], x[2], x[3]))
-    tree.pack()
+    for i in costumer:
+        Label(window, text=costumer[0][i]).pack()
+    # Label(window, text=f"Name:{costumer[0][0]}").pack()
+    # Label(window, text="Costumer information").pack()
+    # costumerInformationColumn = ('Name', 'CPF', 'Sex', 'Birthday')
+    # costumer_view = ttk.Treeview(window, columns=costumerInformationColumn, height=50, selectmode="extended", show='headings')
+    # costumer_view.heading('Name', text="Name")
+    # costumer_view.heading('CPF', text="CPF")
+    # costumer_view.heading('Sex', text="Sex")
+    # costumer_view.heading('Birthday', text="Birthday")
 
-    Label(window, text="Bank information").pack()
-    costumerInformationColumn = ('AccountType', 'Balance')
-    tree = ttk.Treeview(window, columns=costumerInformationColumn, height=200, selectmode="extended", show='headings')
-    tree.heading('AccountType', text="AccountType", anchor=W)
-    tree.heading('Balance', text="Balance", anchor=W)
+    # for x in costumer:
+    #     costumer_view.insert('', 'end', values=(x[0], x[1], x[2], x[3]))
+    # costumer_view.pack()
 
-    for x in account:
-        tree.insert('', 'end', vvalues=(x[0], x[1]))
-    tree.pack()
+    # Label(window, text="Bank information").pack()
+    # bankingInformationColumn = ('AccountType', 'Balance')
+    # banking_view = ttk.Treeview(window, columns=bankingInformationColumn, height=200, selectmode="extended", show='headings')
+    # banking_view.heading('AccountType', text="AccountType", anchor=W)
+    # banking_view.heading('Balance', text="Balance", anchor=W)
 
-    ##### Movimentações #####
-    Label(window, text="Movimentations").pack()
-    ##### Movimentações #####
+    # for x in account:
+    #     banking_view.insert('', 'end', values=(x[0], x[1]))
+    # banking_view.pack()
+
+    # ##### Movimentações #####
+    # Label(window, text="Movimentations").pack()
+    # ##### Movimentações #####
+    # movimentationsColumn = ('Date', 'Description', 'Value')
+    # tree = ttk.Treeview(window, columns=movimentationsColumn, height=200, selectmode="extended", show='headings')
+    # tree.heading('Date', text="Date", anchor=W)
+    # tree.heading('Description', text="Description", anchor=W)
+    # tree.heading('Value', text="Value", anchor=W)
+
+    # for x in movimentations:
+    #     tree.insert('', 'end', values=(x[0], x[1], x[2]))
+    # tree.pack()
 
     ##### Rodapé #####
     ##### Rodapé #####
